9313e318aca8ba4147d85112f961632918591729
eimuc <43643903+eimuc@users.noreply.github.com>
2020-10-05 22:20:46 +0300

    ++

--- js/main.js
+++
@@ -1,7 +0,0 @@
-import time from './data.js';
-import stats from './data.js';
-import renderClock from './renderClock.js';
-import renderProgressBar from './renderProgressBar.js';
-
-renderClock(time);
-renderProgressBar(stats);
